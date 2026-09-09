190a9e101e1740bc528ab88e4ce4f6e5c8b9bc55
Smiley98 <smileysofficialcinema@gmail.com>
2026-09-09 15:07:28 -0400

    Added animation

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -8,14 +8,28 @@ int main()
     InitAudioDevice();
     SetTargetFPS(60);
 
+    float x = 400.0f;
+    float y = 400.0f;
+    float a = 10.0f;    // amplitude (radius of movement)
+    float b = 4.0f;     // frequency (rate of movement)
+
+    Vector2 pos = { 100.0f, 400.0f };
+
     while (!WindowShouldClose())
     {
-        float time = GetTime();
+        float dt = GetFrameTime();
+        float t = GetTime();
         //printf("%f\n", time); <-- need to include cstdio if you want printf, or iostream if you like std::cout <<
         //TraceLog(LOG_INFO, "%f", time); // raylib uses tracelog to output to the console
+        //TraceLog(LOG_INFO, "%f", dt);
+
+        // Simple animation
+        pos += Vector2UnitX * 100.0f * dt;
 
         BeginDrawing();
             ClearBackground(WHITE);
+
+            DrawCircleV(pos, 20.0f, RED);
 
             DrawCircle(50, 100, 25.0f, GREEN);
             DrawCircle(150, 200, 25.0f, DARKGREEN);
@@ -35,6 +49,8 @@ int main()
             DrawText(text, 200, 600, 20, BLUE);
             // TODO -- replace with time
 
+            // TODO -- animate this based on equation in lab document week 1!
+            DrawCircle(x, y, 25.0f, PURPLE);
 
         EndDrawing();
     }
