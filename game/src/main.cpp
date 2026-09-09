f424dd83e8e56f4f33b24937839ffcf54b226dd9
Smiley98 <smileysofficialcinema@gmail.com>
2026-09-09 14:32:54 -0400

    Added text

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -14,6 +14,7 @@ int main()
         ClearBackground(WHITE);
 
         DrawCircleV(GetMousePosition(), 20.0f, RED);
+        DrawText("Connor Smiley 123456", 650, 750, 20, BLUE);
 
         EndDrawing();
     }
